977a953ce9ef769a7e03afa5a4a660e00122409c
Kyle Canamar <canamar.kyle@gmail.com>
2022-04-12 10:35:13 -0700

    creates index page, connects css

--- server.js
+++ server.js
@@ -5,23 +5,23 @@ const express = require('express')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
 const dotenv = require('dotenv')
+dotenv.config()
 const PORT = process.env.PORT || 3001
 const app = express()
 const pokedex = require('./models/pokemon.js')
-dotenv.config()
 //////////////////////
 // Declare Middleware
 //////////////////////
-app.use(methodOverride('_method')) 
-app.use('/public', express.static('public'))
 app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method')) 
+app.use("/static", express.static('public'))
 app.use(morgan('tiny'))
 ///////////////////////
 // Declare Routes and Routers 
 ///////////////////////
 // Index Route
 app.get("/", (req, res) => {
-    res.send("Index, Main Page")
+    res.render('index.ejs', {pokedex: pokedex})
 })
 ///////////////////////////
 // Server Listener
