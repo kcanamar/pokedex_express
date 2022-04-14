306e63b5955a97adf54fd4eaa5ce979d9f80aba2
Kyle Canamar <canamar.kyle@gmail.com>
2022-04-14 10:10:47 -0700

    creates edit button for each pokemon, setup basic edit page

--- server.js
+++ server.js
@@ -20,35 +20,37 @@ app.use(morgan('tiny'))
 // Declare Routes and Routers 
 ///////////////////////
 // Index Route
-app.get("/", (req, res) => {
+app.get("/pokedex", (req, res) => {
     res.render('index.ejs', {pokedex: pokedex})
 })
 // New Route 
-app.get("/new", (req, res) => {
+app.get("/pokedex/new", (req, res) => {
     res.render("new.ejs")
 })
 // Create Route
 // Needs troublshooting as the form is not giving back readable data for my show route to display
-app.post("/", (req, res) => {
+app.post("/pokedex", (req, res) => {
+    // minipulate data to reflect ejs show formatting
+    // { misc: {abilities:}}
     pokedex.unshift(req.body)
-    res.redirect("/")
+    res.send(req.body)
 })
 // Edit Route
-app.get("/edit/:id", (req, res) => {
-    res.send('this is the edit page')
+app.get("/pokedex/edit/:id", (req, res) => {
+    res.render('edit.ejs')
 })
 // Update Route
-// app.put("/:id", (req, res) => {
+// app.put("/pokedex/:id", (req, res) => {
 //     pokedex[parseInt(req.params.id)] = req.body
 //     res.redirect("/")
 // })
 // Delete Route
-app.delete("/:id", (req, res) => {
+app.delete("/pokedex/:id", (req, res) => {
     pokedex.splice(req.params.id, 1)
     res.redirect("/")
 })
 // Show Route
-app.get("/:id", (req, res) => {
+app.get("/pokedex/:id", (req, res) => {
     const pokemon = pokedex[parseInt(req.params.id)]
     res.render("show.ejs", {pokemon})
 })
